9f275ac65528a29fef60c56d72ebcaffd7229a3e
Tammibriggs <Godsfirstbriggs@gmail.com>
2022-04-10 20:02:07 +0100

    fixed userId authorization bug

--- routes/buyCrypto.js
+++ routes/buyCrypto.js
@@ -1,12 +1,12 @@
 const router = require('express').Router()
 const buyRequest = require('../models/BuyCrypto')
 const User = require('../models/User')
-const {verifyToken} = require('./verifyToken')
+const {verifyTokenAndAuthorization, verifyToken} = require('./verifyToken')
 const { sendBuyCryptoAdminEmail, sendBuyCryptoUserEmail } = require('../services')
 
 const validateBuyCryptoInfo = (req, res, next) => {
   const emailValidator = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-  const {name, email, currency, amount, walletAddress, userId} = req.body
+  const {name, email, currency, amount, walletAddress} = req.body
 
   if(!name || typeof name !== 'string'){
     return res.status(401).json({status: 'error', message: 'Invalid name'})
@@ -17,14 +17,14 @@ const validateBuyCryptoInfo = (req, res, next) => {
   else if(!currency || typeof currency !== 'string'){
     return res.status(401).json({status: 'error', message: 'Invalid currency'})
   }
-  else if(!amount || typeof amount !== 'string'){
-    return res.status(401).json({status: 'error', message: 'Invalid amount'})
+  else if(!amount || typeof amount !== 'object'){
+    return res.status(401).json({status: 'error', message: 'Invalid amount. Should be an object'})
+  }
+  else if(!amount.currencyAmount || !amount.nairaAmount){
+    return res.status(401).json({status: 'error', message:'Dollar and Naira amount most be supplied'})
   }
   else if(!walletAddress || typeof walletAddress !== 'string'){
     return res.json({status: 'error', message: 'Invalid wallet address'})
-  }
-  else if(!userId || typeof userId !== 'string'){
-    return res.json({status: 'error', message: 'Invalid userId'})
   }
   else{
     next()
@@ -32,21 +32,22 @@ const validateBuyCryptoInfo = (req, res, next) => {
 }
 
 // CREATE
-router.post('/', validateBuyCryptoInfo, verifyToken, async(req, res) => {
-  const {amount, userId} = req.body
-  const numAmount = Number(amount)
+router.post('/:id', verifyTokenAndAuthorization, validateBuyCryptoInfo, async(req, res) => {
+  const {amount} = req.body
+  const numAmount = Number(amount.nairaAmount)
+
   try{
-    const user = await User.findById(userId)
+    const user = await User.findById(req.params.id)
+    console.log(user)
     const {wallet} = user._doc
-    
     // check if the requested amout is greater that the available amount 
     // then send and error response if the greater than and if not greter than minus 
     // the requested amount the the available amount and upate the database
     if(numAmount > wallet.availableAmount){
       return res.status(422).json({status: 'error', error:'insufficientBalance', message: 'You wallet balance is less that the amount requested'})
     }
-    const currentWalletAmount = wallet.availableAmount - amount
-    await User.updateOne({_id: userId}, {
+    const currentWalletAmount = wallet.availableAmount - numAmount
+    await User.updateOne({_id: req.params.id}, {
       $set: {'wallet.availableAmount' : currentWalletAmount}
     })
 
